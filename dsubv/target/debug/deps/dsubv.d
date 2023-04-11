f937e5fdc43fbfcae1969a127893ecb12012e469
afnleaf <afnleafow@gmail.com>
2023-04-11 16:41:14 -0400

    fixed flushing output

--- dsubv/target/debug/deps/dsubv.d
+++
@@ -1,5 +0,0 @@
-C:\Users\leaf\Code\misc_utils\dsubv\target\debug\deps\dsubv.exe: src\main.rs
-
-C:\Users\leaf\Code\misc_utils\dsubv\target\debug\deps\dsubv.d: src\main.rs
-
-src\main.rs:
--- dsubv/target/debug/dsubv.d
+++
@@ -1,1 +0,0 @@
-C:\Users\leaf\Code\misc_utils\dsubv\target\debug\dsubv.exe: C:\Users\leaf\Code\misc_utils\dsubv\src\main.rs
